abc4ff41db0de754c3a006b285ed3ae2ef3c817d
andbog <aboguta@op.pl>
2018-10-08 02:09:09 +0200

    partial app v2

--- app.py
+++ app.py
@@ -10,54 +10,70 @@ from dash.dependencies import Input, Output, State
 
 from functions import *
 
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = dash.Dash()
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#app = dash.Dash()
 app.config['suppress_callback_exceptions']=True
-
+#app.scripts.config.serve_locally = True
 
 app.layout = html.Div(children = [
                                 html.H1('Simplified* TURF simulator'),
                                 html.P('* only maximal incremental path calculated'),
                                 html.H2('Upload CSV file'),
-                                dcc.Upload(
-                                            id='upload-file',
+                                dcc.Upload(id='upload-file',
                                             children=[html.Button('Upload File')],multiple=False),
                                 html.Hr(),
-                                html.Div(id='el_list',children = [dcc.Checklist(id='symulator',options=[],values=[]),
-                                                                  html.H2(id='total_reach'),
-                                                                  dcc.Graph(id='usage')
-                                                                  ],style={'width':'50%'}),
-                                html.Hr(),
+                                html.Div(id='el_list',style={'width':'50%','float':'left'}),
+                                html.Div(id='incr'),
+                                html.Div(id='incr-graph')
                                 ])
 
 
 
-@app.callback(Output('symulator', 'options'),
+@app.callback(Output('el_list', 'children'),
               [Input('upload-file', 'contents')])
 def update_output(list_of_contents):
     if list_of_contents is not None:
         df = parse_contents(list_of_contents)
-        return [{'label':i, 'value':i} for i in df.columns[2:df.shape[1]]]
+        val = df.columns[2:df.shape[1]]
+        children = [dcc.Checklist(id='symulator',options=[{'label':i, 'value':i} for i in val],values=val,style={'display':'block'}),
+                    dcc.Graph(id='usage',figure={'data':[go.Bar(x=val,y=[100*calc_freq(df,i) for i in val])],
+                                                 'layout':go.Layout(title='Reach of single elements')}),
+                    html.H2(id='total_reach',children=[calculate_reach(df,val)])]
+        return children
 
 @app.callback(Output('total_reach', 'children'),
               [Input('upload-file', 'contents'),Input('symulator', 'values')])
-def update_reach(list_of_contents,vals):
+def update_reach(list_of_contents,val):
     if list_of_contents is not None:
         df = parse_contents(list_of_contents)
-        return calculate_reach(df,vals)
+        return calculate_reach(df,val)
 
 @app.callback(Output('usage', 'figure'),
               [Input('upload-file', 'contents'),Input('symulator', 'values')])
-def update_usage(list_of_contents,vals):
+def update_usage(list_of_contents,val):
     if list_of_contents is not None:
         df = parse_contents(list_of_contents)
-    return {'data':[go.Bar(x=[vals],y=[100*calc_freq(df,i) for i in vals])]
-            ,'layout':go.Layout(title='Reach of single elements')}
+        return {'data':[go.Bar(x=val,y=[100*calc_freq(df,i) for i in val])]
+                ,'layout':go.Layout(title='Reach of single elements')}
 
+@app.callback(Output('incr', 'children'),
+              [Input('upload-file', 'contents')])
+def show_bt(list_of_contents):
+    if list_of_contents is not None:
+        return [html.Button(id='generate_incr',children=['Generate incremental graph'])]
 
-
+@app.callback(Output('incr-graph','children'),
+             [Input('generate_incr','n_clicks')],
+             [State('upload-file', 'contents'),State('symulator', 'values')])
+def gen_inc_graph(clicks,list_of_contents,val):
+    if list_of_contents is not None:
+        df = parse_contents(list_of_contents)
+        temp = calculate_incr(df[val])
+        children = [dcc.Graph(id='increm_graph',figure={'data':[go.Scatter(x=temp[0],y=temp[1],mode='line+markers')],
+                                                        'layout':go.Layout(title='Incremental graph')})]
+        return children
 
 if __name__ == '__main__':
     app.run_server()
--- functions.py
+++ functions.py
@@ -2,18 +2,48 @@ import base64
 import datetime
 import io
 import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-def calc_freq(df,column):
-    total = df.iloc[:,1].sum(axis=0)
+def parse_contents(contents):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    return pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+
+
+def calc_freq(df,column,total=None):
+    if total==None:
+        total = df.iloc[:,1].sum(axis=0)
     count = df[df[column]>0].iloc[:,1].sum(axis=0)
     return count/total
 
 def calculate_reach(df,columns):
     df['suma'] = df[columns].sum(axis=1)
-    return 'Reach: {}%'.format(100*calc_freq(df,'suma'))
+    return 'Reach: {} %'.format(100*calc_freq(df,'suma'))
 
-def parse_contents(contents):
-    content_type, content_string = contents.split(',')
+def calculate_incr(df):
+    labels=[]
+    reach=[]
+    total=df.shape[0]
+    while (df.shape[1]>2):
+        columns=df.columns[2:df.shape[1]]
+        temp_max = 0
 
-    decoded = base64.b64decode(content_string)
-    return pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        for i in columns:
+            a=calc_freq(df,i,total)
+            if a>=temp_max:
+                temp=i
+                temp_max=a
+
+        labels.append(temp)
+        if len(reach)==0:
+            reach.append(temp_max)
+        else:
+            reach.append(reach[len(reach)-1]+temp_max)
+
+        df=df[df[temp]==0]
+
+        df.drop(columns=temp)
+
+    return (labels,reach)
