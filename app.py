51d7c555e3633bfe441d0fa4455b40d37a24e245
andbog <bogutaandrzej@gmail.com>
2018-10-19 23:38:57 +0200

    removed generate graph button, graph now generates on each changes, some changes to style

--- app.py
+++ app.py
@@ -8,14 +8,47 @@ import dash_html_components as html
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
 
-from functions import *
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#app = dash.Dash()
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash()
 app.config['suppress_callback_exceptions']=True
 #app.scripts.config.serve_locally = True
+
+def parse_contents(contents):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    return pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+
+
+def calc_freq(df,column,total=-1):
+    if total<0:
+        total = df.iloc[:,1].sum(axis=0)
+    count = df[df[column]>0].iloc[:,1].sum(axis=0)
+    return count/total
+
+def calculate_reach(df,columns):
+    df['suma'] = df[columns].sum(axis=1)
+    return 'Reach: {} %'.format(100*calc_freq(df,'suma'))
+
+def calculate_incr(df):
+    labels=['no elements']
+    reach=[0]
+    total=df.shape[0]
+
+    while (df.shape[1]>2):
+        columns=df.columns[2:df.shape[1]]
+        temp_max = 0
+        for i in columns:
+            a=calc_freq(df,i,total)
+            if a>=temp_max:
+                temp=i
+                temp_max=a
+        labels.append(temp)
+        reach.append(reach[len(reach)-1]+100*temp_max)
+        df=df[df[temp]==0]
+        df.drop(temp,axis=1,inplace=True)
+
+    return (labels,reach)
 
 app.layout = html.Div(children = [
                                 html.H1('Simplified* TURF simulator'),
@@ -24,11 +57,9 @@ app.layout = html.Div(children = [
                                 dcc.Upload(id='upload-file',
                                             children=[html.Button('Upload File')],multiple=False),
                                 html.Hr(),
-                                html.Div(id='el_list',style={'width':'45%','float':'left'}),
-                                html.Div([html.Div(id='incr'),
-                                html.Div(id='incr-graph')],style={'width':'45%','float':'right'})
-                                ])
-
+                                html.Div(id='el_list',style={'display':'block'}),
+                                html.Div([html.Div(id='ugraph',style={'width':'45%','float':'left','display':'inline'}),
+                                html.Div(id='incr-graph',style={'width':'45%','float':'right','display':'inline'})])])
 
 
 @app.callback(Output('el_list', 'children'),
@@ -38,43 +69,47 @@ def update_output(list_of_contents):
         df = parse_contents(list_of_contents)
         val = df.columns[2:df.shape[1]]
         children = [dcc.Checklist(id='symulator',options=[{'label':i, 'value':i} for i in val],values=val,style={'display':'block'}),
-                    dcc.Graph(id='usage',figure={'data':[go.Bar(x=val,y=[100*calc_freq(df,i) for i in val])],
+                    html.H2(id='total_reach',children=[calculate_reach(df,val)])]
+        return children
+
+@app.callback(Output('ugraph', 'children'),
+              [Input('upload-file', 'contents')])
+def update_output(list_of_contents):
+    if list_of_contents is not None:
+        df = parse_contents(list_of_contents)
+        val = df.columns[2:df.shape[1]]
+        children = [dcc.Graph(id='usage',figure={'data':[go.Bar(x=val,y=[100*calc_freq(df,i) for i in val])],
                                                  'layout':go.Layout(title='Reach of single elements'
                                                  ,yaxis=dict(nticks = 20,
                                                              domain = [0, 100],
                                                              showgrid=True,
                                                              gridcolor='#bdbdbd',
                                                              gridwidth=2,
-                                                             dtick=5,title='Reach'))}),
-
-                    html.H2(id='total_reach',children=[calculate_reach(df,val)])]
+                                                             dtick=5,title='Reach'))})]
         return children
 
 @app.callback(Output('total_reach', 'children'),
-              [Input('upload-file', 'contents'),Input('symulator', 'values')])
-def update_reach(list_of_contents,val):
+              [Input('symulator', 'values')],
+              [State('upload-file', 'contents')])
+def update_reach(val,list_of_contents):
     if list_of_contents is not None:
         df = parse_contents(list_of_contents)
         return calculate_reach(df,val)
 
 @app.callback(Output('usage', 'figure'),
-              [Input('upload-file', 'contents'),Input('symulator', 'values')])
-def update_usage(list_of_contents,val):
+              [Input('symulator', 'values')],
+              [State('upload-file', 'contents')])
+def update_usage(val,list_of_contents):
     if list_of_contents is not None:
         df = parse_contents(list_of_contents)
         return {'data':[go.Bar(x=val,y=[100*calc_freq(df,i) for i in val])]
                 ,'layout':go.Layout(title='Reach of single elements')}
 
-@app.callback(Output('incr', 'children'),
-              [Input('upload-file', 'contents')])
-def show_bt(list_of_contents):
-    if list_of_contents is not None:
-        return [html.Button(id='generate_incr',children=['Generate incremental graph'])]
 
 @app.callback(Output('incr-graph','children'),
-             [Input('generate_incr','n_clicks')],
-             [State('upload-file', 'contents'),State('symulator', 'values')])
-def gen_inc_graph(clicks,list_of_contents,val):
+              [Input('symulator', 'values')],
+              [State('upload-file', 'contents')])
+def gen_inc_graph(val,list_of_contents):
     if list_of_contents is not None:
         df = parse_contents(list_of_contents)
         kolumny=[df.columns[0],df.columns[1]]
